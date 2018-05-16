5090a7bd73b74fe0756c5cbccfeda6355a6eb46c
Martin Pretorius <glasnoster@gmail.com>
2018-05-16 18:55:10 -0300

    Add payment documentation

--- lib/root_insurance/api/payment.rb
+++ lib/root_insurance/api/payment.rb
@@ -1,5 +1,32 @@
 module RootInsurance::Api
   module Payment
+
+    # Create a payment method
+    #
+    # @param [String] policy_holder_id The unique identifier of the policy holder.
+    # @param [String] type The payment method type. Curently only +'debit_order'+ is supported. If omitted, defaults to +'debit_order'+ (optional)
+    # @param [Hash] bank_details Bank details to use for the debit order. See below for details.
+    # @param [String] policy_ids The date on the which the incident occured. (optional)
+    # @return [Hash] An optional list of policy ids to which the payment method should be assigned. (optional)
+    #
+    ## == Bank details
+    # [account_holder (string)]   Name of account holder.
+    # [bank (string)]   Bank name - one of [+absa+, +capitec+, +fnb+, +investec+, +nedbank+, +postbank+, +standard_bank+]
+    # [branch_code (string)]   Branch code for bank account
+    # [account_number (string)]   Bank account number
+    #
+    # @example
+    #  bank_details = {
+    #    first_name:     "Erlich",
+    #    last_name:      "Bachman",
+    #    bank:           "absa",
+    #    branch_code:    "12345",
+    #    account_number: "123456789"
+    #  }
+    #  client.create_payment_method(
+    #          policy_holder_id: "128ba0c0-3f6a-4f8b-9b40-e2066b02b59e",
+    #          bank_details:     bank_details)
+    #
     def create_payment_method(policy_holder_id:, type: 'debit_order', bank_details: {}, policy_ids: nil)
       validate_bank_details(bank_details)
 
@@ -17,6 +44,16 @@ module RootInsurance::Api
       post("policyholders/#{policy_holder_id}/payment-methods", data)
     end
 
+    # Link a payment method to a policy
+    #
+    # @param [String] policy_id The unique identifier of the policy.
+    # @param [String] payment_method_id The unique identifier of the payment method.
+    #
+    # @example
+    #   client.link_payment_method(
+    #     policy_id:         "128ba0c0-3f6a-4f8b-9b40-e2066b02b59e",
+    #     payment_method_id: "e0b7b222-772f-47ac-b08d-c7ba38aa1b25")
+    #
     def link_payment_method(policy_id:, payment_method_id:)
       data = {payment_method_id: payment_method_id}
 
