2e1d77b3a3126e6c93886a8de529bdc36e2ee92f
Martin Pretorius <glasnoster@gmail.com>
2018-05-16 13:39:29 -0300

    Creating an application supports all the modules

--- lib/root_insurance/api/application.rb
+++ lib/root_insurance/api/application.rb
@@ -1,12 +1,24 @@
 module RootInsurance::Api
   module Application
-    def create_application(policyholder_id:, quote_package_id:, monthly_premium:, serial_number: nil)
+    def create_application(policyholder_id:, quote_package_id:, monthly_premium:,
+                           serial_number: nil, spouse_id: nil, children_ids: nil, extended_famliy_ids: nil)
       data = {
         policyholder_id:  policyholder_id,
         quote_package_id: quote_package_id,
-        monthly_premium:  monthly_premium,
-        serial_number:    serial_number
+        monthly_premium:  monthly_premium
       }
+
+      module_data = if serial_number
+        {serial_number: serial_number}
+      elsif spouse_id || children_ids || extended_famliy_ids
+        {
+          spouse_id:           spouse_id,
+          children_ids:        children_ids,
+          extended_famliy_ids: extended_famliy_ids
+        }.reject { |key, value| value.nil? }
+      end
+
+      data = module_data ? data.merge(module_data) : data
 
       post(:applications, data)
     end
--- spec/root_insurance/api/application_spec.rb
+++ spec/root_insurance/api/application_spec.rb
@@ -9,26 +9,71 @@ describe RootInsurance::Api::Application do
   let(:client) { RootInsurance::Client.new(app_id, app_secret, environment) }
 
   describe :create_application do
-    let(:expected_body) do
-      {
-        policyholder_id:  "bf1ada91-eecb-4f47-9bfa-1258bb1e0055",
-        quote_package_id: "f4397823-db4a-4d6a-a06b-08e1a2a3172c",
-        monthly_premium:  50000,
-        serial_number:    "1234567890"
-      }
+    context "using the gadgets module (supplying a serial number)" do
+      let(:expected_body) do
+        {
+          policyholder_id:  "bf1ada91-eecb-4f47-9bfa-1258bb1e0055",
+          quote_package_id: "f4397823-db4a-4d6a-a06b-08e1a2a3172c",
+          monthly_premium:  50000,
+          serial_number:    "1234567890"
+        }
+      end
+
+      it "posts the correct data to the correct endpoint" do
+        stub_request(:post, url)
+          .with(body: expected_body)
+          .to_return(body: "{}")
+
+        client.create_application(
+          policyholder_id:  "bf1ada91-eecb-4f47-9bfa-1258bb1e0055",
+          quote_package_id: "f4397823-db4a-4d6a-a06b-08e1a2a3172c",
+          monthly_premium:  50000,
+          serial_number:    "1234567890")
+      end
     end
 
-    it "posts the correct data to the correct endpoint" do
-      stub_request(:post, url)
-        .with(body: expected_body)
-        .to_return(body: "{}")
+    context "using the funeral cover module" do
+      let(:expected_body) do
+        {
+          policyholder_id:  "bf1ada91-eecb-4f47-9bfa-1258bb1e0055",
+          quote_package_id: "f4397823-db4a-4d6a-a06b-08e1a2a3172c",
+          monthly_premium:  50000,
+          spouse_id:       "1234567890"
+        }
+      end
 
-      client.create_application(
-        policyholder_id:  "bf1ada91-eecb-4f47-9bfa-1258bb1e0055",
-        quote_package_id: "f4397823-db4a-4d6a-a06b-08e1a2a3172c",
-        monthly_premium:  50000,
-        serial_number:    "1234567890")
+      it "posts the correct data to the correct endpoint" do
+        stub_request(:post, url)
+          .with(body: expected_body)
+          .to_return(body: "{}")
+
+        client.create_application(
+          policyholder_id:  "bf1ada91-eecb-4f47-9bfa-1258bb1e0055",
+          quote_package_id: "f4397823-db4a-4d6a-a06b-08e1a2a3172c",
+          monthly_premium:  50000,
+          spouse_id:    "1234567890")
+      end
+    end
+
+    context "using the term module" do
+      let(:expected_body) do
+        {
+          policyholder_id:  "bf1ada91-eecb-4f47-9bfa-1258bb1e0055",
+          quote_package_id: "f4397823-db4a-4d6a-a06b-08e1a2a3172c",
+          monthly_premium:  50000
+        }
+      end
+
+      it "posts the correct data to the correct endpoint" do
+        stub_request(:post, url)
+          .with(body: expected_body)
+          .to_return(body: "{}")
+
+        client.create_application(
+          policyholder_id:  "bf1ada91-eecb-4f47-9bfa-1258bb1e0055",
+          quote_package_id: "f4397823-db4a-4d6a-a06b-08e1a2a3172c",
+          monthly_premium:  50000)
+      end
     end
   end
-
 end
